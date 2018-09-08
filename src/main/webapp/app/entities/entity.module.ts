ba1ca515aee825f3606af1e0694604b63d9dbcda
김종인(Jongin Kim)/Sell개발팀/SKP <kji6252@sk.com>
2018-09-08 19:43:04 +0900

    Puzzle Entity 추가

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { AheaCrawlerPuzzleAheaModule } from './puzzle-ahea/puzzle-ahea.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        AheaCrawlerPuzzleAheaModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
